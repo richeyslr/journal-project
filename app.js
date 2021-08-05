6b629f9877e8354a65d083c8d363b9973d3e673c
Spencer Richey <richeyslr@gmail.com>
2021-08-05 13:02:33 -0700

    functioning edit button

--- app.js
+++ app.js
@@ -26,6 +26,9 @@ addButton.addEventListener("click", getUserEntry);
 let deleteButton = document.getElementById("delete-entry");
 deleteButton.addEventListener("click", getDeleteEntries);
 
+let editButton = document.getElementById("edit-entry");
+editButton.addEventListener("click", getEditEntries);
+
 function getUserEntry(evt) {
   let journalEntries = [];
   let doneEntering = false;
@@ -74,7 +77,7 @@ function postEntries(journalEntries) {
   }
 }
 
-function getDeleteEntries() {
+function getDeleteEntries(evt) {
   let elToDelete = prompt("Which entry would you like to delete?");
   let elToDeleteInt = parseInt(elToDelete);
   for (let i = 0; i < allJournalEntries.length; i++) {
@@ -82,6 +85,54 @@ function getDeleteEntries() {
     let ulToDelete = entries.childNodes[i + 1];
     if (i + 1 === elToDeleteInt) {
       entries.removeChild(ulToDelete);
+    }
+  }
+}
+
+function getEditEntries(evt) {
+  let elToEdit = prompt("Which entry would you like to edit?");
+  let elToEditInt = parseInt(elToEdit);
+  for (let i = 0; i < allJournalEntries.length; i++) {
+    let entries = document.querySelector("#entries");
+    let ulToEdit = entries.childNodes[i + 1];
+    if (i + 1 === elToEditInt) {
+      let entryDate = prompt(`What is today's date?`);
+      if (entryDate != null) {
+        let confidenceLevel = prompt(
+          `What is your confidence level? Enter High, Medium, or Low.`
+        ).toLowerCase();
+        if (
+          confidenceLevel == "high" ||
+          confidenceLevel == "medium" ||
+          confidenceLevel == "low"
+        ) {
+          let journalText = prompt(`What did you learn today?`);
+          if (journalText != null) {
+            let confirmation = confirm(`Do you want to post this entry?`);
+            if (confirmation != false) {
+              let newEditUl = document.createElement("ul");
+              let newDateLi = document.createElement("li");
+              let newConfiLi = document.createElement("li");
+              let newTextLi = document.createElement("li");
+              newDateLi.textContent = `Date: ${entryDate}`;
+              newConfiLi.textContent = `Confidence Level: ${confidenceLevel}`;
+              newTextLi.textContent = `Thoughts: ${journalText}`;
+              newEditUl.appendChild(newDateLi);
+              newEditUl.appendChild(newConfiLi);
+              newEditUl.appendChild(newTextLi);
+              entries.replaceChild(newEditUl, ulToEdit);
+            }
+          } else {
+            doneEntering = true;
+          }
+        } else if (confidenceLevel == null) {
+          doneEntering = true;
+        } else {
+          alert("Enter High, Medium, or Low only!");
+        }
+      } else {
+        doneEntering = true;
+      }
     }
   }
 }
