048f6749fd7fe2fc856d80ada34f7b4b654f8eed
Spencer Richey <richeyslr@gmail.com>
2021-08-05 11:01:38 -0700

    stoppedr repeat entries form rendering

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-jornalEntries = [];
+function JournalEntry(entryDate, confidenceLevel, journalText) {
+  this.dateOf = entryDate;
+  this.confidence = confidenceLevel;
+  this.entryText = journalText;
+}
+JournalEntry.prototype.render = function () {
+  let entryContainer = document.querySelector("#entries");
+  let newEntryUl = document.createElement("ul");
+  let dateLi = document.createElement("li");
+  let confiLi = document.createElement("li");
+  let textLi = document.createElement("li");
+  dateLi.textContent = this.dateOf;
+  confiLi.textContent = this.confidence;
+  textLi.textContent = this.entryText;
+  newEntryUl.appendChild(dateLi);
+  newEntryUl.appendChild(confiLi);
+  newEntryUl.appendChild(textLi);
+  entryContainer.appendChild(newEntryUl);
+};
 
 let addButton = document.getElementById("add-entry");
 addButton.addEventListener("click", getUserEntry);
 
 function getUserEntry(evt) {
+  let journalEntries = [];
   let doneEntering = false;
   while (doneEntering == false) {
     let entryDate = prompt(`What is today's date?`);
@@ -19,14 +38,14 @@ function getUserEntry(evt) {
         let journalText = prompt(`What did you learn today?`);
         if (journalText != null) {
           let confirmation = confirm(`Do you want to post this entry?`);
-          if (confirmation != null) {
+          if (confirmation != false) {
             let newEntry = new JournalEntry(
               entryDate,
               confidenceLevel,
               journalText
             );
-            jornalEntries.push(newEntry);
-            postEntry();
+            journalEntries.push(newEntry);
+            postEntries(journalEntries);
           } else {
             doneEntering = true;
           }
@@ -43,27 +62,10 @@ function getUserEntry(evt) {
     }
   }
 }
-
-function postEntry() {
-  for (let i = 0; i < jornalEntries.length; i++) {
-    let entryContainer = document.querySelector("#entries");
-    let newEntryUl = document.createElement("ul");
-    let dateLi = document.createElement('li');
-    let confiLi = document.createElement('li');
-    let textLi = document.createElement('li');
-    dateLi.textContent = this.dateOf;
-    confiLi.textContent = this.confidence;
-    textLi.textContent = this.entryText;
-    newEntryUl.appendChild(dateLi);
-    newEntryUl.appendChild(confiLi);
-    newEntryUl.appendChild(textLi);
-    entryContainer.appendChild(newEntryUl);
+function postEntries(journalEntries) {
+  for (let i = 0; i < journalEntries.length; i++) {
+    journalEntries[i].render();
   }
 }
-console.log(jornalEntries);
 
-function JournalEntry(entryDate, confidenceLevel, journalText) {
-  this.dateOf = entryDate;
-  this.confidence = confidenceLevel;
-  this.entryText = journalText;
-}
+console.log(journalEntries);
